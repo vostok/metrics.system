ad19aeb1576ae4573eba41761db6a60a656f13b9
Цуп Илья Олегович <tsup@kontur>
2020-07-27 10:25:19 +0500

    WIP: Added host metrics properties for CPU and memory usage.

---
+++ Vostok.Metrics.System/Host/HostMetrics.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Metrics.System.Host
+{
+    [PublicAPI]
+    public class HostMetrics
+    {
+        /// <summary>
+        /// <para>Number of CPU cores utilized by current process.</para>
+        /// <para>This metric has a value between 0 and <see cref="Environment.ProcessorCount"/>.</para>
+        /// <para>This metric is an average value between two observation moments (current and previous).</para>
+        /// </summary>
+        public double CpuUtilizedCores { get; set; }
+
+        /// <summary>
+        /// <para>Fraction of the CPU resources consumed by current process (relative to all cores).</para>
+        /// <para>This metric has a value between 0 and 1.</para>
+        /// <para>This metric is an average value between two observation moments (current and previous).</para>
+        /// </summary>
+        public double CpuUtilizedFraction { get; set; }
+        
+        /// <summary>
+        /// Amount of physical RAM on host.
+        /// </summary>
+        public long MemoryTotal { get; set; }
+        
+        /// <summary>
+        /// Amount of physical RAM available for starting new applications.
+        /// </summary>
+        public long MemoryAvailable { get; set; }
+        
+        /// <summary>
+        /// Amount of physical RAM consumed by system kernel.
+        /// </summary>
+        public long MemoryKernel { get; set; }
+
+        /// <summary>
+        /// Amount of physical RAM used as cache memory.
+        /// </summary>
+        public long MemoryCached { get; set; }
+    }
+}
