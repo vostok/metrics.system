4b822829ce0832b886878ec9e31811631b49a423
Цуп Илья Олегович <tsup@kontur>
2020-07-27 11:40:36 +0500

    WIP: Added metrics collectors without implementations

---
+++ Vostok.Metrics.System/Host/HostMetricsCollector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using JetBrains.Annotations;
+
+namespace Vostok.Metrics.System.Host
+{
+    public class HostMetricsCollector
+    {
+        private readonly Action<HostMetrics> nativeCollector;
+
+        public HostMetricsCollector()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                nativeCollector = new NativeHostMetricsCollector_Windows().Collect;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                nativeCollector = new NativeHostMetricsCollector_Linux().Collect;
+        }
+
+        [NotNull]
+        public HostMetrics Collect()
+        {
+            var metrics = new HostMetrics();
+            CollectNativeMetrics(metrics);
+            throw new NotImplementedException();
+            return metrics;
+        }
+
+        private void CollectNativeMetrics(HostMetrics metrics)
+            => nativeCollector?.Invoke(metrics);
+    }
+}
---
+++ Vostok.Metrics.System/Host/NativeHostMetricsCollector_Linux.cs
@@ -0,0 +1,7 @@
+﻿namespace Vostok.Metrics.System.Host
+{
+    public class NativeHostMetricsCollector_Linux
+    {
+        public void Collect(HostMetrics metrics) { }
+    }
+}
