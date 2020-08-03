3c667747a5fb05edeb827663be8c1b70b4219a13
Цуп Илья Олегович <tolya12345w@gmail.com>
2020-08-03 14:19:48 +0500

    Added simple benchmark

---
+++ Vostok.Metrics.System.Benchmark/MetricsCollectorBenchmark.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using Vostok.Metrics.System.Host;
+using Vostok.Metrics.System.Process;
+
+namespace Vostok.Metrics.System.Benchmark
+{
+    [MemoryDiagnoser]
+    public class MetricsCollectorBenchmark
+    {
+        private readonly CurrentProcessMetricsCollector processCollector = new CurrentProcessMetricsCollector();
+        private readonly HostMetricsCollector hostCollector = new HostMetricsCollector();
+
+        [Benchmark]
+        public void CollectProcessMetrics() => processCollector.Collect();
+
+        [Benchmark]
+        public void CollectHostMetrics() => hostCollector.Collect();
+    }
+}
---
+++ Vostok.Metrics.System.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Vostok.Metrics.System.Benchmark
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<MetricsCollectorBenchmark>();
+        }
+    }
+}
