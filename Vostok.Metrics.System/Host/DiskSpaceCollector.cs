3c409be0d497bed6826f065650a62fad65e32e82
Цуп Илья Олегович <tolya12345w@gmail.com>
2020-08-03 15:44:49 +0500

    Fixed disk space collector

--- Vostok.Metrics.System/Host/DiskSpaceCollector.cs
+++ Vostok.Metrics.System/Host/DiskSpaceCollector.cs
@@ -2,29 +2,56 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using Vostok.Metrics.System.Helpers;
 
 namespace Vostok.Metrics.System.Host
 {
     internal class DiskSpaceCollector
     {
+        private readonly Func<IEnumerable<DriveInfo>, IEnumerable<DriveInfo>> additionalDiskFilter;
+        private readonly Func<string, string> nameFormatter;
+
+        public DiskSpaceCollector()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                additionalDiskFilter = FilterDisks_Windows;
+                nameFormatter = FormatDiskName_Windows;
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                additionalDiskFilter = FilterDisks_Linux;
+                nameFormatter = FormatDiskName_Linux;
+            }
+        }
+
         public void Collect(HostMetrics metrics)
         {
-            metrics.DiskSpaceInfos = GetDiskSpaceInfos().ToDictionary(x => x.Name);
+            var diskSpaceInfos = new Dictionary<string, DiskSpaceInfo>();
+
+            foreach (var info in GetDiskSpaceInfos())
+            {
+                if (!diskSpaceInfos.ContainsKey(info.Name))
+                    diskSpaceInfos[info.Name] = info;
+            }
+
+            metrics.DiskSpaceInfos = diskSpaceInfos;
         }
 
         private IEnumerable<DiskSpaceInfo> GetDiskSpaceInfos()
         {
-            foreach (var drive in DriveInfo.GetDrives().Where(x => x.IsReady && x.DriveType == DriveType.Fixed))
+            foreach (var drive in additionalDiskFilter(DriveInfo.GetDrives().Where(x => x.IsReady && x.DriveType == DriveType.Fixed)))
             {
                 var result = new DiskSpaceInfo();
 
                 try
                 {
-                    result.Name = drive.Name.Replace(":\\", string.Empty);
+                    result.Name = nameFormatter(drive.Name);
                     result.FreeBytes = drive.TotalFreeSpace;
                     result.TotalCapacityBytes = drive.TotalSize;
-                    result.FreePercent = drive.TotalFreeSpace * 100d / drive.TotalSize;
+                    if (result.TotalCapacityBytes != 0)
+                        result.FreePercent = result.FreeBytes * 100d / result.TotalCapacityBytes;
                 }
                 catch (Exception error)
                 {
@@ -35,5 +62,13 @@ namespace Vostok.Metrics.System.Host
                 yield return result;
             }
         }
+
+        private IEnumerable<DriveInfo> FilterDisks_Linux(IEnumerable<DriveInfo> disks) => disks.Where(x => x.Name.Contains("/dev/sd"));
+
+        private IEnumerable<DriveInfo> FilterDisks_Windows(IEnumerable<DriveInfo> disks) => disks;
+
+        private string FormatDiskName_Windows(string diskName) => diskName.Replace(":\\", string.Empty);
+
+        private string FormatDiskName_Linux(string diskName) => diskName.Replace("/dev/", string.Empty);
     }
 }
