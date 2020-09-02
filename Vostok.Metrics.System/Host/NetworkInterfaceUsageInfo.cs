531c4bd4d96278d270f5a3f2fe31223aaa4bdf7e
Цуп Илья Олегович <tolya12345w@gmail.com>
2020-09-02 15:51:34 +0500

    Added NetworkInterfaceUsageInfo

---
+++ Vostok.Metrics.System/Host/NetworkInterfaceUsageInfo.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+
+namespace Vostok.Metrics.System.Host
+{
+    [PublicAPI]
+    public class NetworkInterfaceUsageInfo
+    {
+        public string InterfaceName { get; set; }
+        public long SentBytesPerSecond { get; set; }
+        public long ReceivedBytesPerSecond { get; set; }
+        public long BandwidthBitsPerSecond { get; set; }
+    }
+}
