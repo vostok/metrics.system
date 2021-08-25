fb8a8a3df5a34006636b9d0b0705d9741019899b
Ilya Shumilin <shumilin@skbkontur.ru>
2021-08-25 11:43:38 +0500

    add dns monitor extensions

---
+++ Vostok.Metrics.System/Dns/DnsMonitorExtensions_Logging.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.Commons.Time;
+using Vostok.Logging.Abstractions;
+
+namespace Vostok.Metrics.System.Dns
+{
+    [PublicAPI]
+    public static class DnsMonitorExtensions_Logging
+    {
+        /// <summary>
+        /// <para>Enables logging of all dns lookups matched by given <paramref name="filter"/> into provided <paramref name="log"/> with Info level.</para>
+        /// <para>Dispose of the returned <see cref="IDisposable"/> object to stop the logging.</para>
+        /// </summary>
+        [NotNull]
+        public static IDisposable LogLookups([NotNull] this DnsMonitor monitor, [NotNull] ILog log, [CanBeNull] Predicate<DnsLookupInfo> filter)
+            => monitor.Subscribe(new LoggingObserver(log, filter));
+
+        private class LoggingObserver : IObserver<DnsLookupInfo>
+        {
+            private readonly ILog log;
+            private readonly Predicate<DnsLookupInfo> filter;
+
+            public LoggingObserver(ILog log, Predicate<DnsLookupInfo> filter)
+            {
+                this.log = log.ForContext<DnsMonitor>();
+                this.filter = filter ?? (_ => true);
+            }
+
+            public void OnNext(DnsLookupInfo lookupInfo)
+            {
+                if (!filter(lookupInfo))
+                    return;
+
+                log.Info(
+                    "Dns lookup occured with duration = {LookupDuration}. " +
+                    "Successfully: {Successfully}",
+                    new
+                    {
+                        LookupDuration = lookupInfo.Latency.ToPrettyString(),
+                        Successfully = !lookupInfo.IsFailed
+                    });
+            }
+
+            public void OnError(Exception error)
+                => log.Warn(error);
+
+            public void OnCompleted()
+            {
+            }
+        }
+    }
+}
---
+++ Vostok.Metrics.System/Dns/DnsMonitorExtensions_Metrics.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.Metrics.Primitives.Counter;
+using Vostok.Metrics.Primitives.Timer;
+
+namespace Vostok.Metrics.System.Dns
+{
+    [PublicAPI]
+    public static class DnsMonitorExtensions_Metrics
+    {
+        /// <summary>
+        /// <para>Enables reporting of following dns lookups metrics:</para>
+        /// <list type="bullet">
+        ///     <item><description>Percentile of time to resolve dns lookup.</description></item>
+        ///     <item><description>Total lookups count (sum of all lookups for the reporting period).</description></item>
+        ///     <item><description>Failed lookups count(sum of all failed lookups for the reporting period).</description></item>
+        /// </list>
+        /// <para>Note that provided <see cref="IMetricContext"/> should contain tags sufficient to decouple these metrics from others.</para>
+        /// <para>Dispose of the returned <see cref="IDisposable"/> object to stop reporting metrics.</para>
+        /// </summary>
+        [NotNull]
+        public static IDisposable ReportMetrics([NotNull] this DnsMonitor monitor, [NotNull] IMetricContext metricContext, TimeSpan? period = null)
+            => monitor.Subscribe(new ReportingObserver(metricContext, period));
+
+        private class ReportingObserver : IObserver<DnsLookupInfo>
+        {
+            private readonly ITimer lookupLatency;
+            private readonly ICounter lookupCount;
+            private readonly ICounter failedLookupCount;
+
+            public ReportingObserver(IMetricContext metricContext, TimeSpan? period)
+            {
+                lookupLatency = metricContext.CreateSummary("DnsLookupLatency", new SummaryConfig {Unit = WellKnownUnits.Milliseconds, ScrapePeriod = period});
+
+                var counterConfig = new CounterConfig {Unit = WellKnownUnits.None, ScrapePeriod = period};
+                lookupCount = metricContext.CreateCounter("DnsLookupCount", counterConfig);
+                failedLookupCount = metricContext.CreateCounter("FailedDnsLookupCount", counterConfig);
+            }
+
+            public void OnNext(DnsLookupInfo value)
+            {
+                lookupCount.Increment();
+                if (value.IsFailed)
+                    failedLookupCount.Increment();
+
+                lookupLatency.Report(value.Latency.TotalMilliseconds);
+            }
+
+            public void OnError(Exception error)
+            {
+            }
+
+            public void OnCompleted()
+            {
+            }
+        }
+    }
+}
