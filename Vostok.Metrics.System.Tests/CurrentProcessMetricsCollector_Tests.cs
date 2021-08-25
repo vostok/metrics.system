a815437ac61fd29dd1c053ebef7884c650b6cbc9
Ilya Shumilin <shumilin@skbkontur.ru>
2021-08-25 11:46:37 +0500

    add socket monitor tests

--- Vostok.Metrics.System.Tests/CurrentProcessMetricsCollector_Tests.cs
+++ Vostok.Metrics.System.Tests/CurrentProcessMetricsCollector_Tests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -17,6 +20,7 @@ namespace Vostok.Metrics.System.Tests
         [SetUp]
         public void TestSetup()
         {
+            collector?.Dispose();
             collector = new CurrentProcessMetricsCollector();
             collector.Collect();
         }
@@ -109,6 +113,79 @@ namespace Vostok.Metrics.System.Tests
             metrics.GcGen1Collections.Should().BeGreaterThan(0);
             metrics.GcGen2Collections.Should().BeGreaterThan(0);
             metrics.GcHeapSize.Should().BeGreaterThan(0);
+        }
+
+        [Test]
+        public void Should_measure_outgoing_socket_connections()
+        {
+            var client = new HttpClient();
+
+            client.GetAsync("https://www.google.com/").GetAwaiter().GetResult();
+
+            var metrics = collector.Collect();
+
+            metrics.OutgoingConnectionsCount.Should().Be(1);
+            metrics.IncomingConnectionsCount.Should().Be(0);
+            metrics.FailedConnectionsCount.Should().Be(0);
+        }
+
+        [Test]
+        public void Should_measure_incoming_socket_connections()
+        {
+            const int connectionCount = 10;
+            var ipEndPoint = new IPEndPoint(IPAddress.IPv6Loopback, 11000);
+
+            var sListener = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+
+            sListener.Bind(ipEndPoint);
+            sListener.Listen(10);
+
+            Parallel.For(0,
+                connectionCount,
+                i =>
+                {
+                    var sSender = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+                    sSender.Connect(ipEndPoint);
+                });
+
+            for (var i = 0; i < connectionCount; i++)
+            {
+                sListener.Accept();
+            }
+
+            var metrics = collector.Collect();
+
+            metrics.OutgoingConnectionsCount.Should().Be(connectionCount);
+            metrics.IncomingConnectionsCount.Should().Be(connectionCount);
+            metrics.FailedConnectionsCount.Should().Be(0);
+        }
+
+        [Test]
+        public void Should_measure_failed_socket_connections()
+        {
+            const int connectionCount = 10;
+            var wrongEndPoint = new IPEndPoint(IPAddress.IPv6Loopback, 11000);
+
+            Parallel.For(0,
+                connectionCount,
+                i =>
+                {
+                    var sSender = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+                    try
+                    {
+                        sSender.Connect(wrongEndPoint);
+                    }
+                    catch
+                    {
+                        // Ignore
+                    }
+                });
+
+            var metrics = collector.Collect();
+
+            metrics.FailedConnectionsCount.Should().Be(connectionCount);
+            metrics.OutgoingConnectionsCount.Should().Be(connectionCount);
+            metrics.IncomingConnectionsCount.Should().Be(0);
         }
     }
 }
