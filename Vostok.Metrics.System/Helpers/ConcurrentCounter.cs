aa26134503d1475cbd7cc17a0991ca1dafd1c74e
Ilya Shumilin <shumilin@skbkontur.ru>
2021-09-01 11:41:48 +0500

    add dns observer

---
+++ Vostok.Metrics.System/Helpers/ConcurrentCounter.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+namespace Vostok.Metrics.System.Helpers
+{
+    internal class ConcurrentCounter
+    {
+        private volatile int value;
+
+        public void Increment() => Interlocked.Increment(ref value);
+
+        public int CollectAndReset() => Interlocked.Exchange(ref value, 0);
+    }
+}
