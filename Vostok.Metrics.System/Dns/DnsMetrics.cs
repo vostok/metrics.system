f60456b7a968e06bb67ea63d381031b44f63e613
Ilya Shumilin <shumilin@skbkontur.ru>
2021-09-09 14:54:46 +0500

    Bumped version to 0.3.1.

---
+++ Vostok.Metrics.System/Dns/DnsMetrics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Metrics.System.Dns
+{
+    [PublicAPI]
+    public class DnsMetrics
+    {
+        public DnsMetrics(long lookupsCount, long failedLookups, long averageLookupDuration)
+        {
+            LookupsCount = lookupsCount;
+            FailedLookups = failedLookups;
+            AverageLookupDuration = averageLookupDuration;
+        }
+        
+        public long LookupsCount { get; }
+        
+        public long FailedLookups { get; }
+        
+        public long AverageLookupDuration { get; }
+    }
+}
