a9a656fe458f41f6b4a7d8213be7b0df13dd06be
Ilya Tzoop <ilyatzoop@gmail.com>
2021-10-01 11:06:59 +0500

    Added DisposablePeriodicObservable

---
+++ Vostok.Metrics.System/Helpers/DisposablePeriodicObservable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Vostok.Metrics.System.Helpers
+{
+    public class DisposablePeriodicObservable<T> : IObservable<T>, IDisposable
+    {
+        private readonly PeriodicObservable<T> periodicObservable;
+        private readonly Action onDispose;
+
+        public DisposablePeriodicObservable(TimeSpan period, Func<T> factory, Action onDispose)
+        {
+            periodicObservable = new PeriodicObservable<T>(period, factory);
+            this.onDispose = onDispose;
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer) => periodicObservable.Subscribe(observer);
+
+        public void Dispose()
+        {
+            onDispose();
+        }
+    }
+}
