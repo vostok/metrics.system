79d96c9a6af622f59a26a0766223fac802681f21
Ilya Tzoop <ilyatzoop@gmail.com>
2021-10-01 11:14:09 +0500

    Make HostMonitor and CurrentProcessMonitor IDisposable

--- Vostok.Metrics.System/Process/CurrentProcessMonitor.cs
+++ Vostok.Metrics.System/Process/CurrentProcessMonitor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Vostok.Metrics.System.Helpers;
 
@@ -9,20 +10,51 @@ namespace Vostok.Metrics.System.Process
     /// <see cref="CurrentProcessMonitor"/> provides means to observe <see cref="CurrentProcessMetrics"/> periodically.
     /// </summary>
     [PublicAPI]
-    public class CurrentProcessMonitor
+    public class CurrentProcessMonitor : IDisposable
     {
-        private readonly ConcurrentDictionary<TimeSpan, PeriodicObservable<CurrentProcessMetrics>> observables 
-            = new ConcurrentDictionary<TimeSpan, PeriodicObservable<CurrentProcessMetrics>>();
+        private readonly object guard = new object();
 
         private readonly CurrentProcessMetricsSettings settings;
+
+        private Dictionary<TimeSpan, DisposablePeriodicObservable<CurrentProcessMetrics>> observables
+            = new Dictionary<TimeSpan, DisposablePeriodicObservable<CurrentProcessMetrics>>();
 
         public CurrentProcessMonitor(CurrentProcessMetricsSettings settings)
             => this.settings = settings;
 
         public CurrentProcessMonitor()
-            : this (new CurrentProcessMetricsSettings()) { }
+            : this(new CurrentProcessMetricsSettings())
+        {
+        }
 
         public IObservable<CurrentProcessMetrics> ObserveMetrics(TimeSpan period)
-            => observables.GetOrAdd(period, p => new PeriodicObservable<CurrentProcessMetrics>(p, new CurrentProcessMetricsCollector(settings).Collect));
+        {
+            lock (guard)
+            {
+                if (observables == null)
+                    throw new ObjectDisposedException(nameof(CurrentProcessMonitor));
+
+                return observables.GetOrAdd(period, BuildObservable);
+            }
+        }
+
+        public void Dispose()
+        {
+            Task.Run(() =>
+            {
+                lock (guard)
+                {
+                    foreach (var disposablePeriodicObservable in observables)
+                        disposablePeriodicObservable.Value.Dispose();
+                    observables = null;
+                }
+            });
+        }
+
+        private DisposablePeriodicObservable<CurrentProcessMetrics> BuildObservable(TimeSpan period)
+        {
+            var collector = new CurrentProcessMetricsCollector(settings);
+            return new DisposablePeriodicObservable<CurrentProcessMetrics>(period, collector.Collect, collector.Dispose);
+        }
     }
 }
