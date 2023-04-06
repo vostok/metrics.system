e1ec6fef4c9a60acff1ae4e35d9b531e9528c773
dgorlov <dgorlov@skbkontur.ru>
2023-04-06 14:49:10 +0500

    renames

--- Vostok.Metrics.System/Helpers/Linux/ProcStatReader.cs
+++ Vostok.Metrics.System/Helpers/Linux/ProcStatReader.cs
@@ -13,111 +13,91 @@ namespace Vostok.Metrics.System.Helpers.Linux;
 /// </summary>
 internal readonly struct ProcStatReader : IDisposable
 {
+    private const string file = "/proc/vmstat";
+
     public ProcStatReader()
     {
 #if NET6_0_OR_GREATER
-        systemStatReader = new SpanReusableFileReader("/proc/stat");
+        reader = new SpanReusableFileReader(file);
 #else
-        systemStatReader = new ReusableFileReader("/proc/stat");
+        reader = new ReusableFileReader(file);
 #endif
     }
 
-    public ProcStatReader(bool useDotnetCpuCount, Stream procStatStream)
+    public ProcStatReader(Stream procStatStream)
     {
 #if NET6_0_OR_GREATER
-        systemStatReader = new SpanReusableFileReader(procStatStream);
+        reader = new SpanReusableFileReader(procStatStream);
 #else
-        systemStatReader = new ReusableFileReader(procStatStream);
+        reader = new ReusableFileReader(procStatStream);
 #endif
     }
 
 #if NET6_0_OR_GREATER
-    private readonly SpanReusableFileReader systemStatReader;
+    private readonly SpanReusableFileReader reader;
 #else
-    private readonly ReusableFileReader systemStatReader; //2.7k
+    private readonly ReusableFileReader reader; //2.7k
 #endif
 
-    public bool TryRead(out ProcStat procStat)
+    public bool TryRead(out ProcStat value)
     {
-        procStat = new ProcStat();
+        value = new ProcStat();
 #if NET6_0_OR_GREATER
-        systemStatReader.Reset();
-        var index = 0;
-        //NOTE тут особо длинная строка intr
-        while (systemStatReader.TryReadLine(out var line))
-        {
-            if (index == 0)
-            {
-                if (!TryParseTimes(line, ref procStat))
-                {
-                    procStat = default;
-                    return false;
-                }
-            }
-            else if (!line.StartsWith("cpu".AsSpan()))
-                continue;
+        reader.Reset();
 
-            index++;
-
-            break;
-        //    if (useDotnetCpuCount)
-        //        break;
-        }
-
-        //if (useDotnetCpuCount)
-        //    procStat.CpuCount = Environment.ProcessorCount;
-        //else
-        //    procStat.CpuCount = index - 1;
-        return index >= 1;
+        if (!reader.TryReadLine(out var line)
+            || !TryParseTimes(line, ref value))
+            return false;
+        return true;
 #else
-        return ParseNotOptimized(ref procStat);
+        return ParseNotOptimized(ref value);
 #endif
     }
 
 #if NET6_0_OR_GREATER
 
-    private static bool TryParseTimes(ReadOnlySpan<char> line, ref ProcStat ss)
+    private static bool TryParseTimes(ReadOnlySpan<char> line, ref ProcStat value)
     {
         var e = ProcFsParserHelper.EnumerateTokens(line);
 
         if (!e.TryMove(0) || !e.Token.SequenceEqual("cpu"))
             return false;
 
-        if (!e.TryMove(1) || !ulong.TryParse(e.Token, out ss.UserTime))
+        if (!e.TryMove(1) || !ulong.TryParse(e.Token, out value.UserTime))
             return false;
-        if (!e.TryMove(2) || !ulong.TryParse(e.Token, out ss.NicedTime))
+        if (!e.TryMove(2) || !ulong.TryParse(e.Token, out value.NicedTime))
             return false;
-        if (!e.TryMove(3) || !ulong.TryParse(e.Token, out ss.SystemTime))
+        if (!e.TryMove(3) || !ulong.TryParse(e.Token, out value.SystemTime))
             return false;
-        if (!e.TryMove(4) || !ulong.TryParse(e.Token, out ss.IdleTime))
+        if (!e.TryMove(4) || !ulong.TryParse(e.Token, out value.IdleTime))
             return false;
         return true;
     }
 #else
-    private bool ParseNotOptimized(ref ProcStat procStat)
+    private bool ParseNotOptimized(ref ProcStat value)
     {
         var correctReads = 0;
-        if (FileParser.TrySplitLine(systemStatReader.ReadFirstLine(), 5, out var parts) && parts[0] == "cpu")
+        if (FileParser.TrySplitLine(reader.ReadFirstLine(), 5, out var parts) && parts[0] == "cpu")
         {
             if (ulong.TryParse(parts[1], out var utime))
             {
                 correctReads++;
-                procStat.UserTime = utime;
+                value.UserTime = utime;
             }
 
-            if (ulong.TryParse(parts[2], out procStat.NicedTime))
+            if (ulong.TryParse(parts[2], out value.NicedTime))
                 correctReads++;
 
             if (ulong.TryParse(parts[3], out var stime))
             {
                 correctReads++;
-                procStat.SystemTime = stime;
+                value.SystemTime = stime;
             }
 
             if (ulong.TryParse(parts[4], out var itime))
             {
                 correctReads++;
-                procStat.IdleTime = itime;
+                value.IdleTime = itime;
             }
         }
 
@@ -127,6 +107,6 @@ internal readonly struct ProcStatReader : IDisposable
 
     public void Dispose()
     {
-        systemStatReader?.Dispose();
+        reader?.Dispose();
     }
 }
